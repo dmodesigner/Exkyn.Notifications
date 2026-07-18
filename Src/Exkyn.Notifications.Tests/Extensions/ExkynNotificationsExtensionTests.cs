e0b95535963f08f46db7b04f18b84354401d0fda
Daniel Moura <daniel.dmo@live.com>
2026-07-18 16:49:21 -0300

    Adicionado a classe de teste para validar ExkynNotificaitonsExtension

--- Src/Exkyn.Notifications.Tests/Extensions/ExkynNotificationsExtensionTests.cs
+++ Src/Exkyn.Notifications.Tests/Extensions/ExkynNotificationsExtensionTests.cs
@@ -1,6 +1,28 @@
-﻿namespace Exkyn.Notifications.Tests.Extensions;
+﻿using Exkyn.Notifications.Abstractions;
+using Exkyn.Notifications.Extensions;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Exkyn.Notifications.Tests.Extensions;
 
 public class ExkynNotificationsExtensionTests
 {
-    
+    [Fact]
+    public void AddExkynNotifications_ShouldReturnBuilderWithCorrectServices()
+    {
+        //Arrange
+        var services = new ServiceCollection();
+        
+        //Act
+        var builder = services.AddExkynNotifications();
+        
+        //Assert
+        builder.Should().NotBeNull();
+        
+        //Garante que o objeto retornado implementa a interface correta
+        builder.Should().BeAssignableTo<IExkynNotificationsBuilder>();
+        
+        //Garante que a coleção de serviços dentro do builder é a mesma coleção que foi criado na etapa Arrange
+        builder.Services.Should().BeSameAs(services);
+    }
 }
